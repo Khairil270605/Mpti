4893cb65860b5668f57baf61884fd40f9f81d234
AufaHamdy <aufahamdykesuma@gmail.com>
2025-07-16 18:10:07 +0700

    memperbaiki header atas cetak invoice

--- customer_invoice_cetak.php
+++ customer_invoice_cetak.php
@@ -38,7 +38,7 @@
 			<div>
 
 				<center>
-					<h3>Toko Online Pakaian Tenun</h3>
+					<h3>Toko Online Bincy Jaya Abadi</h3>
 				</center>
 
 				<h4>INVOICE-00<?php echo $i['invoice_id'] ?></h4>
